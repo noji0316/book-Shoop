1859829a694f0555a6f13b5fd6ddd836b87f0137
Caleb <njh2694@gmail.com>
2024-10-17 17:20:58 +0900

    전체 도서 조회

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,8 +1,11 @@
+const ensureAuthorization = require('../auth'); // 인증 모듈
+const jwt = require('jsonwebtoken');
 const conn = require('../mariadb'); // db 모듈
 const {StatusCodes} = require('http-status-codes'); // status code 모듈
 
 // (카테고리 별, 신간 여부) 전체 도서 목록 조회
 const allBooks = (req,res) => {
+    let allbooksRes = {};
     let { category_id, news, limit, currentPage } = req.query;
 
     // limit : page 당 도서 수
@@ -11,7 +14,8 @@ const allBooks = (req,res) => {
 
     let offset = limit * (currentPage-1);
 
-    let sql = "SELECT * FROM books";
+
+    let sql = "SELECT SQL_CALC_FOUND_ROWS *, (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes FROM books";
     let values = [];
      if (category_id && news) {
         sql += " WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
@@ -24,39 +28,99 @@ const allBooks = (req,res) => {
     }
         sql += " LIMIT ? OFFSET ?";
         values.push(parseInt(limit), offset)
+        conn.query(sql, values, 
+            (err, results) => {
+            if(err) {
+                console.log(err);
+                // return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            console.log(results);
+            if(results.length)
+                allbooksRes.books = results;
+            else
+                return res.status(StatusCodes.NOT_FOUND).end();
+        })
 
+        sql = "SELECT found_rows()";
+        values.push(parseInt(limit), offset)
+        conn.query(sql, 
+            (err, results) => {
+            if(err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+                let pagination = {};
+                pagination.currentPage = parseInt(currentPage);
+                pagination.totalCount = results[0]["found_rows()"];
+
+                allbooksRes.pagination = pagination;
+
+                return res.status(StatusCodes.OK).json(allbooksRes);
+        })
+    };
+
+const bookDetail = (req,res) => {
+
+    // 로그인 상태가 아니면 => liked 빼고 보내주면 되고
+    // 로그인 상태이면 => liked 추가해서
+    let authorization = ensureAuthorization(req, res);
+    
+    if(authorization instanceof jwt.TokenExpiredError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            "message" : "로그인 세션이 만료되었으니 다시 로그인 해주십시오!"
+        });
+    } else if (authorization instanceof jwt.JsonWebTokenError) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            "message" : "잘못된 토큰입니다"
+        });
+    } else if (authorization instanceof ReferenceError){
+        let book_id = req.params.id;
+
+        let sql = `SELECT *,
+                        (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes
+                    FROM books 
+                    LEFT JOIN category 
+                    ON books.category_id = category.category_id 
+                    WHERE books.id=?;`;
+        let values = [book_id];
+        conn.query(sql, values, 
+        (err, results) => {
+        if(err) {
+        console.log(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+        if(results[0])
+        return res.status(StatusCodes.OK).json(results[0]);
+        else
+        return res.status(StatusCodes.NOT_FOUND).end();
+        })
+
+
+    } else {
+        let book_id = req.params.id;
+
+        let sql = `SELECT *,
+                            (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes,
+                            (SELECT EXISTS (SELECT * FROM likes WHERE user_id=? AND liked_book_id=?)) AS liked
+                        FROM books 
+                        LEFT JOIN category 
+                        ON books.category_id = category.category_id 
+                        WHERE books.id=?;`;
+        let values = [authorization.id, book_id, book_id];
         conn.query(sql, values, 
             (err, results) => {
             if(err) {
                 console.log(err);
                 return res.status(StatusCodes.BAD_REQUEST).end();
             }
-            if(results.length)
-                return res.status(StatusCodes.OK).json(results);
+            if(results[0])
+                return res.status(StatusCodes.OK).json(results[0]);
             else
                 return res.status(StatusCodes.NOT_FOUND).end();
         })
-    }
-
-    
-
-const bookDetail = (req,res) => {
-    let {id} = (req.params)
-
-    let sql = `SELECT * FROM books LEFT JOIN category 
-                ON books.category_id = category.id WHERE books.id=?`;
-    conn.query(sql, id, 
-        (err, results) => {
-        if(err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
-        if(results[0])
-            return res.status(StatusCodes.OK).json(results[0]);
-        else
-            return res.status(StatusCodes.NOT_FOUND).end();
-    })
-};
+      }
+    };
 
 
 
